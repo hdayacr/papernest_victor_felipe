b1c4c6c279dde48520ef980c05fade581bb6fe76
vjfn1990 <vjfn1990@gmail.com>
2019-01-11 18:21:45 +0100

    feature: functions.py added and also the one_out_of_two function

---
+++ Django/functions.py
@@ -0,0 +1,7 @@
+def one_out_of_two(string):
+    for index in range(0, len(string)):
+        if index % 2 == 0:
+            string = string[:index]+string[index].upper()+string[index+1:]
+        else:
+            string = string[:index]+string[index].lower()+string[index+1:]
+    return string
